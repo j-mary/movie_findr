40cac6d56a694d3456f9caeb9b214aebdb6a8798
j-mary <ochejejude@gmail.com>
2023-06-05 09:49:14 +0100

    Remove unused code, setup app theme and flow

---
+++ lib/features/movie_flow/movie_flow.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MovieFlow extends StatefulWidget {
+  const MovieFlow({ Key? key }) : super(key: key);
+
+  @override
+  MovieFlowState createState() => MovieFlowState();
+}
+
+class MovieFlowState extends State<MovieFlow> {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      
+    );
+  }
+}
